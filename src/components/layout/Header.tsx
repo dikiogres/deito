31f08dc334a58a6420982445d81e6af9a5504b91
ryohilmi <ryodm.74@gmail.com>
2022-12-04 13:28:35 +0700

    feat: add logo on Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,9 @@
+import Image from 'next/image';
 import * as React from 'react';
 
 import UnstyledLink from '@/components/links/UnstyledLink';
+
+import Logo from '~/Logo.png';
 
 const links = [
   { href: '/', label: 'Route 1' },
@@ -9,11 +12,13 @@ const links = [
 
 export default function Header() {
   return (
-    <header className='sticky top-0 z-50 bg-white'>
+    <header className='sticky top-0 z-50 bg-slate-800 text-white'>
       <div className='layout flex h-14 items-center justify-between'>
-        <UnstyledLink href='/' className='font-bold hover:text-gray-600'>
-          Home
-        </UnstyledLink>
+        <div className='relative flex'>
+          <UnstyledLink href='/' className='font-bold hover:text-gray-600'>
+            <Image src={Logo} alt='Logo' width={80} />
+          </UnstyledLink>
+        </div>
         <nav>
           <ul className='flex items-center justify-between space-x-4'>
             {links.map(({ href, label }) => (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,21 @@
 import Image from 'next/image';
 import * as React from 'react';
 
+import Header from '@/components/layout/Header';
 import Layout from '@/components/layout/Layout';
 import ArrowLink from '@/components/links/ArrowLink';
 import UnderlineLink from '@/components/links/UnderlineLink';
 import Seo from '@/components/Seo';
 
 import Logo from '~/Logo.png';
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
-
-// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
-// Before you begin editing, follow all comments with `STARTERCONF`,
-// to customize the default configuration.
 
 export default function HomePage() {
   return (
     <Layout>
       {/* <Seo templateTitle='Home' /> */}
       <Seo />
+
+      <Header />
 
       <main>
         <section className='bg-white'>
