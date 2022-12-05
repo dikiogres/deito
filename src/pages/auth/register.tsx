0b5180f45216404b1e2501afaf3eeed85b015c3a
dikiogres <dikiogres@gmail.com>
2022-12-05 20:56:59 +0700

    feat: register page

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,25 +1,30 @@
+import Link from 'next/link';
 import React from 'react';
+
+import BgImg from '~/images/auth/registerBackground.png';
 
 const Register = () => {
   return (
     <div
       className='min-h-screen py-40'
-      // style={{
-      //   background-image: 'linear-gradient(115deg, #9F7AEA, #FEE2FE)'
-      //   }}
+      style={{
+        backgroundImage: 'linear-gradient(115deg, #E97991, #20202B)',
+      }}
     >
       <div className='container mx-auto'>
         <div className='mx-auto flex w-10/12 flex-col overflow-hidden rounded-xl bg-white shadow-lg lg:w-8/12 lg:flex-row'>
           <div
             className='flex w-full flex-col items-center justify-center bg-cover bg-center bg-no-repeat p-12 lg:w-1/2'
-            // style="{{background-image: url('images/Register-Background.png');}}"
+            style={{
+              backgroundImage: `url(${BgImg.src})`,
+            }}
           >
             <h1 className='mb-3 text-3xl text-white'>Welcome</h1>
             <div>
               <p className='text-white'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
                 suspendisse aliquam varius rutrum purus maecenas ac{' '}
-                <a href='#' className='font-semibold text-purple-500'>
+                <a href='#' className='font-semibold text-pink-400'>
                   Learn more
                 </a>
               </p>
@@ -31,21 +36,23 @@ const Register = () => {
               Create your account. It’s free and only take a minute
             </p>
             <form action='#'>
-              <div className='grid grid-cols-2 gap-5'>
+              <div className='mt-5'>
                 <input
                   type='text'
-                  placeholder='Firstname'
-                  className='border border-gray-400 py-1 px-2'
-                />
-                <input
-                  type='text'
-                  placeholder='Surname'
-                  className='border border-gray-400 py-1 px-2'
+                  placeholder='Name'
+                  className='w-full border border-gray-400 py-1 px-2'
                 />
               </div>
               <div className='mt-5'>
                 <input
                   type='text'
+                  placeholder='Phone Number'
+                  className='w-full border border-gray-400 py-1 px-2'
+                />
+              </div>
+              <div className='mt-5'>
+                <input
+                  type='email'
                   placeholder='Email'
                   className='w-full border border-gray-400 py-1 px-2'
                 />
@@ -59,29 +66,35 @@ const Register = () => {
               </div>
               <div className='mt-5'>
                 <input
-                  type='password'
-                  placeholder='Confirm Password'
-                  className='w-full border border-gray-400 py-1 px-2'
+                  type='checkbox'
+                  className='mr-5 border border-gray-400'
                 />
-              </div>
-              <div className='mt-5'>
-                <input type='checkbox' className='border border-gray-400' />
                 <span>
                   I accept the{' '}
-                  <a href='#' className='font-semibold text-purple-500'>
+                  <a href='#' className='font-semibold text-pink-400'>
                     Terms of Use
                   </a>{' '}
                   &{' '}
-                  <a href='#' className='font-semibold text-purple-500'>
+                  <a href='#' className='font-semibold text-pink-400'>
                     Privacy Policy
                   </a>
                 </span>
               </div>
               <div className='mt-5'>
-                <button className='w-full bg-purple-500 py-3 text-center text-white'>
+                <button className='w-full rounded-xl bg-pink-400 py-3 text-center font-semibold text-white shadow-lg transition duration-500 hover:scale-110 hover:shadow-2xl'>
                   Register Now
                 </button>
               </div>
+              <p className='mt-2 mb-0 pt-1 text-sm font-semibold'>
+                Already have an account?
+                <Link
+                  href='/auth/login'
+                  className='text-red-600 transition duration-200 ease-in-out hover:text-red-700 focus:text-red-700'
+                >
+                  {' '}
+                  Log in
+                </Link>
+              </p>
             </form>
           </div>
         </div>
