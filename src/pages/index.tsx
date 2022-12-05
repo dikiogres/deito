95577ec519fde9a840cabd02b8e464d001d6c6ff
dikiogres <dikiogres@gmail.com>
2022-12-05 22:09:36 +0700

    feat: aos on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
-import * as React from 'react';
+import AOS from 'aos';
+import React, { useEffect } from 'react';
+
+import 'aos/dist/aos.css';
 
 import Layout from '@/components/layout/Layout';
 import Seo from '@/components/Seo';
@@ -7,14 +10,26 @@ import Showcase from '@/container/home/Showcase';
 import Waiting from '@/container/home/Waiting';
 
 export default function HomePage() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <Layout>
       {/* <Seo templateTitle='Home' /> */}
       <Seo />
       <main>
-        <Hero />
-        <Waiting />
-        <Showcase />
+        <section className='relative overflow-hidden bg-slate-900'>
+          <div data-aos='fade-down' data-aos-duration='1000'>
+            <Hero />
+          </div>
+          <div data-aos='fade-up' data-aos-duration='1000'>
+            <Waiting />
+          </div>
+          <div data-aos='fade-up' data-aos-duration='1000'>
+            <Showcase />
+          </div>
+        </section>
       </main>
     </Layout>
   );
