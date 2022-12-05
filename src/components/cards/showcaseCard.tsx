00408429db73d9d30435e9092da9bd6a5cc2eb04
dikiogres <dikiogres@gmail.com>
2022-12-05 19:42:30 +0700

    feat: dynamic card content

--- src/components/cards/showcaseCard.tsx
+++ src/components/cards/showcaseCard.tsx
@@ -1,19 +1,15 @@
 import Image from 'next/image';
 import React from 'react';
 
-import Profile1 from '~/images/profile1.jpg';
+import { Profile } from '@/types/profile.type';
 
-const showcaseCard = () => {
+const showcaseCard = ({ item }: { item: Profile }) => {
   return (
     <div className='hover:shadow-3xl max-w-sm overflow-hidden rounded bg-white shadow-lg transition duration-500 hover:scale-110'>
-      <Image className='w-full' src={Profile1} alt='Profile' />
+      <Image className='w-full' src={item.img} alt='Profile' />
       <div className='px-6 py-4'>
-        <div className='mb-2 text-xl font-bold'>Bonnie Green</div>
-        <p className='text-base text-gray-700'>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-          quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium
-          nihil.
-        </p>
+        <div className='mb-2 text-xl font-bold'>{item.name}</div>
+        <p className='text-base text-gray-700'>{item.description}</p>
         <div className='mt-2 flex flex-col space-x-3 md:mt-4'>
           <div className='flex items-center'>
             <svg
@@ -27,27 +23,21 @@ const showcaseCard = () => {
               <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z'></path>
             </svg>
             <p className='ml-2 text-sm font-bold text-gray-900 dark:text-white'>
-              4.95
+              {item.rating}
             </p>
             <span className='mx-1.5 h-1 w-1 rounded-full bg-gray-500 dark:bg-gray-400'></span>
             <a
               href='#'
               className='text-sm font-medium text-gray-900 underline hover:no-underline dark:text-white'
             >
-              73 reviews
+              {item.reviews} reviews
             </a>
           </div>
         </div>
       </div>
       <div className='px-6 pt-4 pb-2'>
         <span className='mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700'>
-          #photography
-        </span>
-        <span className='mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700'>
-          #travel
-        </span>
-        <span className='mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700'>
-          #winter
+          {item.interest}
         </span>
       </div>
     </div>
