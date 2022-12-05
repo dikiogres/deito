7474dc8ca302bc864e832960c8e96d206247fd4a
dikiogres <dikiogres@gmail.com>
2022-12-05 17:21:54 +0700

    feat: waiting section

--- src/container/home/Waiting.tsx
+++ src/container/home/Waiting.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
 const Waiting = () => {
-  return <div>Waiting</div>;
+  const tags = [
+    'Movie',
+    'Emotional Support',
+    'Karaoke',
+    'Cosplay',
+    'Aquarium',
+    'Night Ride',
+  ];
+
+  return (
+    <section className='w-full bg-slate-900 py-14 text-white'>
+      <div className='mx-auto  max-w-6xl'>
+        <p className='py-6 text-center text-xl'>
+          100,000 Deits are waiting for you
+        </p>
+        <div className='grid grid-cols-3'>
+          {tags.map((tag) => (
+            <div
+              key={tag}
+              className='mx-6 mb-6 rounded-2xl bg-slate-800 px-6 py-3 text-left text-xl hover:bg-slate-700'
+            >
+              {tag}
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Waiting;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,42 +2,17 @@ import * as React from 'react';
 
 import Layout from '@/components/layout/Layout';
 import Seo from '@/components/Seo';
-import Hero from '@/container/home/Hero';
+import Hero from '@/container/Hero';
+import Waiting from '@/container/home/Waiting';
 
 export default function HomePage() {
-  const tags = [
-    'Movie',
-    'Emotional Support',
-    'Karaoke',
-    'Cosplay',
-    'Aquarium',
-    'Night Ride',
-  ];
-
   return (
     <Layout>
       {/* <Seo templateTitle='Home' /> */}
       <Seo />
-
       <main>
         <Hero />
-        <section className='w-full bg-slate-900 py-14 text-white'>
-          <div className='mx-auto max-w-6xl'>
-            <p className='py-6 text-center text-xl'>
-              100,000 Deits are waiting for you
-            </p>
-            <div className='grid grid-cols-3'>
-              {tags.map((tag) => (
-                <div
-                  key={tag}
-                  className='mx-6 mb-6 rounded-2xl bg-slate-800 px-6 py-3 text-left text-xl hover:bg-slate-700'
-                >
-                  {tag}
-                </div>
-              ))}
-            </div>
-          </div>
-        </section>
+        <Waiting />
       </main>
     </Layout>
   );
