8d44df6110fc3b25bc7341cd712d8870a50f731a
dikiogres <dikiogres@gmail.com>
2022-12-05 18:04:19 +0700

    feat: home page structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import * as React from 'react';
 import Layout from '@/components/layout/Layout';
 import Seo from '@/components/Seo';
 import Hero from '@/container/Hero';
+import Service from '@/container/home/Service';
+import Showcase from '@/container/home/Showcase';
 import Waiting from '@/container/home/Waiting';
 
 export default function HomePage() {
@@ -13,6 +15,8 @@ export default function HomePage() {
       <main>
         <Hero />
         <Waiting />
+        <Service />
+        <Showcase />
       </main>
     </Layout>
   );
