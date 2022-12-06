f4518cfb66aa11d7f6b2ce4d103c0dbd163de90c
dikiogres <dikiogres@gmail.com>
2022-12-07 00:01:49 +0700

    feat: wrap login  with react hook form provider

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,48 +1,35 @@
 import AOS from 'aos';
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
 
 import 'aos/dist/aos.css';
 
 import BgImg from '~/images/auth/registerBackground.png';
 import Logo from '~/Logo.png';
 
+interface LoginType {
+  email: string;
+  password: string;
+}
+
 const Login = () => {
-  //const user = useContext(AuthContext);
-
-  const emailRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
-
   useEffect(() => {
     AOS.init();
   }, []);
 
-  // const createAccount = async () => {
-  //   try {
-  //     await auth.createUserWithEmailAndPassword(
-  //       emailRef.current!.value,
-  //       passwordRef.current!.value
-  //     );
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
+  const methods = useForm<LoginType>({ mode: 'onBlur' });
 
-  const signIn = async () => {
-    // try {
-    //   await auth.signInWithEmailAndPassword(
-    //     emailRef.current!.value,
-    //     passwordRef.current!.value
-    //   );
-    // } catch (error) {
-    //   console.error(error);
-    // }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = methods;
+
+  const onSubmit = async (data: LoginType) => {
+    console.log(data);
   };
-
-  // const signOut = async () => {
-  //   await auth.signOut();
-  // };
 
   return (
     <div
@@ -63,83 +50,93 @@ const Login = () => {
               <Image src={Logo} alt='Logo' width={128} />
             </Link>
             <p className='mb-4'>Please log in to your account</p>
-            <form action='#'>
-              <div className='mt-5'>
-                <input
-                  type='email'
-                  placeholder='Email'
-                  className='w-full border border-gray-400 py-1 px-2'
-                  ref={emailRef}
-                />
-              </div>
-              <div className='mt-5'>
-                <input
-                  type='password'
-                  placeholder='Password'
-                  className='w-full border border-gray-400 py-1 px-2'
-                  ref={passwordRef}
-                />
-              </div>
-              <div className='my-5 flex items-center justify-between'>
-                <div className='form-group form-check'>
+            <FormProvider {...methods}>
+              <form action='#' onSubmit={handleSubmit(onSubmit)}>
+                <div className='mt-5'>
                   <input
-                    type='checkbox'
-                    className='form-check-input float-left mt-1 mr-2 h-4 w-4 cursor-pointer appearance-none rounded-sm border border-gray-300 bg-white bg-contain bg-center bg-no-repeat align-top transition duration-200 checked:border-blue-600 checked:bg-blue-600 focus:outline-none'
-                    id='exampleCheck2'
+                    type='email'
+                    placeholder='Email'
+                    className='w-full border border-gray-400 py-1 px-2'
                   />
-                  <label className='form-check-label inline-block text-gray-800'>
-                    Remember me
-                  </label>
                 </div>
-                <a href='#!' className='text-gray-800 underline'>
-                  Forgot password?
-                </a>
-              </div>
-              <div className='mt-5'>
-                <button
-                  className='w-full rounded-xl bg-pink-400 py-3 text-center font-semibold text-white shadow-lg transition duration-500 hover:scale-110 hover:shadow-2xl'
-                  onClick={signIn}
-                >
-                  Log In
-                </button>
-              </div>
-              <div className='my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-gray-300 after:mt-0.5 after:flex-1 after:border-t after:border-gray-300'>
-                <p className='mx-4 mb-0 text-center font-semibold'>OR</p>
-              </div>
-
-              <a
-                className='mb-3 flex w-full items-center justify-center rounded px-7 py-3  text-sm font-medium uppercase leading-snug text-white shadow-lg transition duration-500 hover:scale-110 hover:shadow-2xl'
-                style={{
-                  backgroundColor: '#3b5998',
-                }}
-                href='#!'
-                role='button'
-                data-mdb-ripple='true'
-                data-mdb-ripple-color='light'
-              >
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  viewBox='0 0 320 512'
-                  className='mr-2 h-3.5 w-3.5'
-                >
-                  <path
-                    fill='currentColor'
-                    d='M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z'
+                <div className='mt-5'>
+                  <input
+                    type='password'
+                    {...register('email', { required: 'Email is required' })}
+                    placeholder='Password'
+                    className='w-full border border-gray-400 py-1 px-2'
                   />
-                </svg>
-                Continue with Facebook
-              </a>
-              <p className='mt-2 mb-0 pt-1 text-sm font-semibold'>
-                Don't have an account?
-                <Link
-                  href='/auth/register'
-                  className='text-red-600 transition duration-200 ease-in-out hover:text-red-700 focus:text-red-700'
+                  {errors.email && (
+                    <p className='text-red-400'>{errors.email.message}</p>
+                  )}
+                </div>
+                <div className='my-5 flex items-center justify-between'>
+                  <div className='form-group form-check'>
+                    <input
+                      type='checkbox'
+                      {...register('password', {
+                        required: 'Password is required',
+                      })}
+                      className='form-check-input float-left mt-1 mr-2 h-4 w-4 cursor-pointer appearance-none rounded-sm border border-gray-300 bg-white bg-contain bg-center bg-no-repeat align-top transition duration-200 checked:border-blue-600 checked:bg-blue-600 focus:outline-none'
+                      id='exampleCheck2'
+                    />
+                    {errors.password && (
+                      <p className='text-red-400'>{errors.password.message}</p>
+                    )}
+                    <label className='form-check-label inline-block text-gray-800'>
+                      Remember me
+                    </label>
+                  </div>
+                  <a href='#!' className='text-gray-800 underline'>
+                    Forgot password?
+                  </a>
+                </div>
+                <div className='mt-5'>
+                  <button
+                    type='submit'
+                    className='w-full rounded-xl bg-pink-400 py-3 text-center font-semibold text-white shadow-lg transition duration-500 hover:scale-110 hover:shadow-2xl'
+                  >
+                    Log In
+                  </button>
+                </div>
+                <div className='my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-gray-300 after:mt-0.5 after:flex-1 after:border-t after:border-gray-300'>
+                  <p className='mx-4 mb-0 text-center font-semibold'>OR</p>
+                </div>
+
+                <a
+                  className='mb-3 flex w-full items-center justify-center rounded px-7 py-3  text-sm font-medium uppercase leading-snug text-white shadow-lg transition duration-500 hover:scale-110 hover:shadow-2xl'
+                  style={{
+                    backgroundColor: '#3b5998',
+                  }}
+                  href='#!'
+                  role='button'
+                  data-mdb-ripple='true'
+                  data-mdb-ripple-color='light'
                 >
-                  {' '}
-                  Register
-                </Link>
-              </p>
-            </form>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 320 512'
+                    className='mr-2 h-3.5 w-3.5'
+                  >
+                    <path
+                      fill='currentColor'
+                      d='M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z'
+                    />
+                  </svg>
+                  Continue with Facebook
+                </a>
+                <p className='mt-2 mb-0 pt-1 text-sm font-semibold'>
+                  Don't have an account?
+                  <Link
+                    href='/auth/register'
+                    className='text-red-600 transition duration-200 ease-in-out hover:text-red-700 focus:text-red-700'
+                  >
+                    {' '}
+                    Register
+                  </Link>
+                </p>
+              </form>
+            </FormProvider>
           </div>
           <div
             className='flex w-full flex-col items-center justify-center bg-cover bg-center bg-no-repeat p-12 lg:w-1/2'
