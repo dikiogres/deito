890a30846c60ae4f1f1e8d58c3b3f88f68b6630a
dikiogres <dikiogres@gmail.com>
2022-12-06 18:46:12 +0700

    feat: import type and dynamic content

--- src/components/cards/serviceCard.tsx
+++ src/components/cards/serviceCard.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const ServiceCard = () => {
+import { Service } from '@/types/service.type';
+
+const ServiceCard = ({ item }: { item: Service }) => {
   return (
     <div
       className='flex h-64 w-48 rounded-lg p-6 shadow-lg transition duration-500 hover:scale-110 hover:shadow-2xl dark:border-gray-700 dark:bg-gray-800'
       style={{
-        backgroundImage: 'url(https://placekeanu.com/250/g)',
+        backgroundImage: `${item.img}`,
       }}
     >
       <div className='flex items-end'>
@@ -13,7 +15,7 @@ const ServiceCard = () => {
           href='#'
           className='inline-flex items-center rounded-lg bg-[#00000075] px-3 py-2 text-center text-sm font-medium text-white'
         >
-          1+ Deits
+          {item.dater}+ Deits
         </a>
       </div>
     </div>
