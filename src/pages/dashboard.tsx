16e696ca332ab1b0b8aed77438857bdff654df2c
dikiogres <dikiogres@gmail.com>
2022-12-06 23:51:32 +0700

    feat: dashboard page

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DashboardPage = () => {
+  return (
+    <div className='container mx-auto flex py-2'>
+      <div className='mx-auto mt-24 overflow-y-hidden px-12 py-24 text-gray-600'>
+        <h2 className='text-2xl font-semibold'>You are logged in!</h2>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
