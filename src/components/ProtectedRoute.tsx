67fcce0eb57b06ce12dd6d65b53593f6b442cf18
dikiogres <dikiogres@gmail.com>
2022-12-07 00:32:20 +0700

    feat: create HOC for protected route

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
 
-const ProtectedRoute = () => {
-  return <div>ProtectedRoute</div>;
+import { useAuth } from '../auth/context/AuthContext';
+
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (!user.uid) {
+      router.push('/login');
+    }
+  }, [router, user]);
+
+  return <div>{user ? children : null}</div>;
 };
 
 export default ProtectedRoute;
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import ProtectedRoute from '@/components/ProtectedRoute';
+
 const DashboardPage = () => {
   return (
-    <div className='container mx-auto flex py-2'>
-      <div className='mx-auto mt-24 overflow-y-hidden px-12 py-24 text-gray-600'>
-        <h2 className='text-2xl font-semibold'>You are logged in!</h2>
+    <ProtectedRoute>
+      <div className='container mx-auto flex py-2'>
+        <div className='mx-auto mt-24 overflow-y-hidden px-12 py-24 text-gray-600'>
+          <h2 className='text-2xl font-semibold'>You are logged in!</h2>
+        </div>
       </div>
-    </div>
+    </ProtectedRoute>
   );
 };
 
