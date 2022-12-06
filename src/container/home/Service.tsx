a9e756bf1071b81161020adc6d67d2067de9217f
dikiogres <dikiogres@gmail.com>
2022-12-06 18:13:56 +0700

    feat: static swiper card

--- src/container/home/Service.tsx
+++ src/container/home/Service.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import SwiperCore, { Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 const Service = () => {
+  SwiperCore.use([Autoplay]);
+
   return (
     <section className='w-full bg-slate-900 py-14 text-white'>
       <div className='mx-auto  max-w-6xl'>
@@ -8,6 +13,139 @@ const Service = () => {
           Service is available on more than 100+ cities around the world
         </p>
       </div>
+      <Swiper
+        // install Swiper modules
+        modules={[Autoplay]}
+        freeMode={true}
+        spaceBetween={0}
+        grabCursor={true}
+        slidesPerView={7}
+        autoplay={{
+          delay: 1,
+          disableOnInteraction: false,
+        }}
+        speed={5000}
+      >
+        <SwiperSlide>
+          <div
+            className='flex h-64 w-48 rounded-lg p-6 shadow-md dark:border-gray-700 dark:bg-gray-800'
+            style={{
+              backgroundImage: 'url(https://placekeanu.com/250/g)',
+            }}
+          >
+            <div className='flex items-end'>
+              <a
+                href='#'
+                className='inline-flex items-center rounded-lg bg-[#00000075] px-3 py-2 text-center text-sm font-medium text-white'
+              >
+                1+ Deits
+              </a>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className='flex h-64 w-48 rounded-lg p-6 shadow-md dark:border-gray-700 dark:bg-gray-800'
+            style={{
+              backgroundImage: 'url(https://placekeanu.com/260/g)',
+            }}
+          >
+            <div className='flex items-end'>
+              <a
+                href='#'
+                className='inline-flex items-center rounded-lg bg-[#00000075] px-3 py-2 text-center text-sm font-medium text-white'
+              >
+                2+ Deits
+              </a>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className='flex h-64 w-48 rounded-lg p-6 shadow-md dark:border-gray-700 dark:bg-gray-800'
+            style={{
+              backgroundImage: 'url(https://placekeanu.com/270/g)',
+            }}
+          >
+            <div className='flex items-end'>
+              <a
+                href='#'
+                className='inline-flex items-center rounded-lg bg-[#00000075] px-3 py-2 text-center text-sm font-medium text-white'
+              >
+                3+ Deits
+              </a>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className='flex h-64 w-48 rounded-lg p-6 shadow-md dark:border-gray-700 dark:bg-gray-800'
+            style={{
+              backgroundImage: 'url(https://placekeanu.com/280/g)',
+            }}
+          >
+            <div className='flex items-end'>
+              <a
+                href='#'
+                className='inline-flex items-center rounded-lg bg-[#00000075] px-3 py-2 text-center text-sm font-medium text-white'
+              >
+                4+ Deits
+              </a>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className='flex h-64 w-48 rounded-lg p-6 shadow-md dark:border-gray-700 dark:bg-gray-800'
+            style={{
+              backgroundImage: 'url(https://placekeanu.com/290/g)',
+            }}
+          >
+            <div className='flex items-end'>
+              <a
+                href='#'
+                className='inline-flex items-center rounded-lg bg-[#00000075] px-3 py-2 text-center text-sm font-medium text-white'
+              >
+                5+ Deits
+              </a>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className='flex h-64 w-48 rounded-lg p-6 shadow-md dark:border-gray-700 dark:bg-gray-800'
+            style={{
+              backgroundImage: 'url(https://placekeanu.com/240/g)',
+            }}
+          >
+            <div className='flex items-end'>
+              <a
+                href='#'
+                className='inline-flex items-center rounded-lg bg-[#00000075] px-3 py-2 text-center text-sm font-medium text-white'
+              >
+                6+ Deits
+              </a>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className='flex h-64 w-48 rounded-lg p-6 shadow-md dark:border-gray-700 dark:bg-gray-800'
+            style={{
+              backgroundImage: 'url(https://placekeanu.com/230/g)',
+            }}
+          >
+            <div className='flex items-end'>
+              <a
+                href='#'
+                className='inline-flex items-center rounded-lg bg-[#00000075] px-3 py-2 text-center text-sm font-medium text-white'
+              >
+                7+ Deits
+              </a>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </section>
   );
 };
